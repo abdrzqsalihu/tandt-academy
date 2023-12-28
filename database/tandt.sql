50606b31a287728247ff8cc93b8c77ac1b678ff1
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-12-28 13:25:55 +0100

    updated the db file

--- database/tandt.sql
+++ database/tandt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 26, 2023 at 11:04 PM
+-- Generation Time: Dec 28, 2023 at 01:25 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
