d7f79ef88e18da7c4d9960a2444c8adb508a7169
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-12-28 23:54:51 +0100

    updated the db file

--- database/tandt.sql
+++ database/tandt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2023 at 01:25 PM
+-- Generation Time: Dec 28, 2023 at 11:54 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,7 +41,168 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `name`, `email`, `phone`, `password`, `status`) VALUES
-(1, 'Abdulrazaq Salihu', 'user@admin.com', '', '$2y$10$KnxPwE0kgbZbMK48/ToMe.26q4LRmDkXiGjaGD0H.Wp/7bWpDi1Ry', 'SUPER_ADMIN');
+(1, 'Abdulrazaq Salihu', 'user@admin.com', '', '123456', 'SUPER_ADMIN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic1`
+--
+
+CREATE TABLE `basic1` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `subject_abr` varchar(200) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic1_subjects`
+--
+
+CREATE TABLE `basic1_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic2`
+--
+
+CREATE TABLE `basic2` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic2_subjects`
+--
+
+CREATE TABLE `basic2_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic3`
+--
+
+CREATE TABLE `basic3` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic3_subjects`
+--
+
+CREATE TABLE `basic3_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic4`
+--
+
+CREATE TABLE `basic4` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic4_subjects`
+--
+
+CREATE TABLE `basic4_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic5`
+--
+
+CREATE TABLE `basic5` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `basic5_subjects`
+--
+
+CREATE TABLE `basic5_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -73,6 +234,301 @@ INSERT INTO `classes` (`id`, `classes`) VALUES
 (12, 'SSS 1'),
 (13, 'SSS 2'),
 (14, 'SSS 3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss1`
+--
+
+CREATE TABLE `jss1` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss1_subjects`
+--
+
+CREATE TABLE `jss1_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss2`
+--
+
+CREATE TABLE `jss2` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss2_subjects`
+--
+
+CREATE TABLE `jss2_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss3`
+--
+
+CREATE TABLE `jss3` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jss3_subjects`
+--
+
+CREATE TABLE `jss3_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kg1`
+--
+
+CREATE TABLE `kg1` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kg1_subjects`
+--
+
+CREATE TABLE `kg1_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kg1_subjects`
+--
+
+INSERT INTO `kg1_subjects` (`id`, `subject`, `subject_abr`, `subject_teacher`) VALUES
+(1, 'English Language', 'ENG', 'Musa Dikko');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nursery2`
+--
+
+CREATE TABLE `nursery2` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nursery2_subjects`
+--
+
+CREATE TABLE `nursery2_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pre_nursery`
+--
+
+CREATE TABLE `pre_nursery` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pre_nursery_subjects`
+--
+
+CREATE TABLE `pre_nursery_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss1`
+--
+
+CREATE TABLE `sss1` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss1_subjects`
+--
+
+CREATE TABLE `sss1_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss2`
+--
+
+CREATE TABLE `sss2` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss2_subjects`
+--
+
+CREATE TABLE `sss2_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss3`
+--
+
+CREATE TABLE `sss3` (
+  `id` int(11) NOT NULL,
+  `subject` varchar(100) NOT NULL,
+  `first_ca` int(11) NOT NULL,
+  `second_ca` int(11) NOT NULL,
+  `third_ca` int(11) NOT NULL,
+  `fourth_ca` int(11) NOT NULL,
+  `exam` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `grade` varchar(1) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sss3_subjects`
+--
+
+CREATE TABLE `sss3_subjects` (
+  `id` int(200) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `subject_abr` varchar(255) NOT NULL,
+  `subject_teacher` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -110,9 +566,177 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `basic1`
+--
+ALTER TABLE `basic1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic1_subjects`
+--
+ALTER TABLE `basic1_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic2`
+--
+ALTER TABLE `basic2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic2_subjects`
+--
+ALTER TABLE `basic2_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic3`
+--
+ALTER TABLE `basic3`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic3_subjects`
+--
+ALTER TABLE `basic3_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic4`
+--
+ALTER TABLE `basic4`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic4_subjects`
+--
+ALTER TABLE `basic4_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic5`
+--
+ALTER TABLE `basic5`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `basic5_subjects`
+--
+ALTER TABLE `basic5_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `classes`
 --
 ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss1`
+--
+ALTER TABLE `jss1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss1_subjects`
+--
+ALTER TABLE `jss1_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss2`
+--
+ALTER TABLE `jss2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss2_subjects`
+--
+ALTER TABLE `jss2_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss3`
+--
+ALTER TABLE `jss3`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jss3_subjects`
+--
+ALTER TABLE `jss3_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kg1`
+--
+ALTER TABLE `kg1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kg1_subjects`
+--
+ALTER TABLE `kg1_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nursery2`
+--
+ALTER TABLE `nursery2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nursery2_subjects`
+--
+ALTER TABLE `nursery2_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pre_nursery`
+--
+ALTER TABLE `pre_nursery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pre_nursery_subjects`
+--
+ALTER TABLE `pre_nursery_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss1`
+--
+ALTER TABLE `sss1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss1_subjects`
+--
+ALTER TABLE `sss1_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss2`
+--
+ALTER TABLE `sss2`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss2_subjects`
+--
+ALTER TABLE `sss2_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss3`
+--
+ALTER TABLE `sss3`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sss3_subjects`
+--
+ALTER TABLE `sss3_subjects`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -132,10 +756,124 @@ ALTER TABLE `admin`
   MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `basic1`
+--
+ALTER TABLE `basic1`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic1_subjects`
+--
+ALTER TABLE `basic1_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic2`
+--
+ALTER TABLE `basic2`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic2_subjects`
+--
+ALTER TABLE `basic2_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic3`
+--
+ALTER TABLE `basic3`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic3_subjects`
+--
+ALTER TABLE `basic3_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic4`
+--
+ALTER TABLE `basic4`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic4_subjects`
+--
+ALTER TABLE `basic4_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic5`
+--
+ALTER TABLE `basic5`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `basic5_subjects`
+--
+ALTER TABLE `basic5_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
   MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `jss1_subjects`
+--
+ALTER TABLE `jss1_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jss2_subjects`
+--
+ALTER TABLE `jss2_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `jss3_subjects`
+--
+ALTER TABLE `jss3_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `kg1_subjects`
+--
+ALTER TABLE `kg1_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `nursery2_subjects`
+--
+ALTER TABLE `nursery2_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pre_nursery_subjects`
+--
+ALTER TABLE `pre_nursery_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sss1_subjects`
+--
+ALTER TABLE `sss1_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sss2_subjects`
+--
+ALTER TABLE `sss2_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sss3_subjects`
+--
+ALTER TABLE `sss3_subjects`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `students`
